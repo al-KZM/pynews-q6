d5ebd45cd8e74cd0e7e01611f8e907f632e21b6b
x41lakazam <x41lakazam@gmail.com>
2021-03-21 22:59:35 +0200

    rout

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,5 +1,6 @@
 import flask_wtf
 import wtforms
+from wtforms import validators as vld
 
 # Form class --> flask_wtf.FlaskForm
 # Form fields --> wtforms.SomethingField
@@ -7,14 +8,15 @@ import wtforms
 # each form is a class inheriting from flask_wtf.FlaskForm
 # every class attribute is a field in the form
 
-# For example:
-# A form with two fields:
-#   - A name
-#   - An age
-class TestForm(flask_wtf.FlaskForm):
+class QueryForm(flask_wtf.FlaskForm):
 
-    name = wtforms.StringField("Name: ")
-    age  = wtforms.IntegerField("Age: ")
+    query = wtforms.StringField("Query: ", validators=[vld.DataRequired(message="Input something..")])
 
-    submit = wtforms.SubmitField("Submit the form !")
+    submit = wtforms.SubmitField("Search")
 
+
+class SignUpForm(flask_wtf.FlaskForm):
+    username = wtforms.StringField("Username: ")
+    password = wtforms.PasswordField("Password: ", validators=[vld.Length(6, 12)])
+
+    submit = wtforms.SubmitField("Sign up")
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,15 +1,73 @@
 import flask
 
 from . import app       # . is webapp/
-from . import forms
+from . import forms, news_functions
 
 @app.route("/")
 def home():
     return "Hello world !"
 
-@app.route('/test-form', methods=["GET", "POST"])
-def test():
-    # Create an instance of the form
-    form = forms.TestForm()
+## Create a route:
+# Form with one single field "query"
+# Use the form data to display some articles about the query
 
-    return flask.render_template("test_form.html", form=form)
+# 1) Create the form        v
+# 2) Create the route
+# 3) Create the template that displays the form
+# 4) Create the template that displays the articles
+
+@app.route("/article/query/<query>")
+def query_article(query):
+    articles = news_functions.get_news(query)
+    return flask.render_template("articles.html", articles=articles)
+
+@app.route("/search-article", methods=["GET", "POST"])
+def search_article():
+    form = forms.QueryForm()
+
+    # case 1: Post request --> The user is sending data
+    if flask.request.method == "POST":
+        if form.validate_on_submit(): # Check all the validators
+            url = flask.url_for("query_article", query=form.query.data)
+            # url --> /article/query/rick
+            return flask.redirect(url)
+
+
+    # case 2: Get request --> the user just wants to see the page
+    return flask.render_template("search_article.html", form=form)
+
+# Sign up page
+
+# Create a page that displays a sign up form (username & password) and when it gets data from a user
+# just print "Rick is signing up with password chocolate"
+
+# The password needs to contain 6 to 12 characters
+@app.route("/sign-up", methods=["GET","POST"])
+def signup():
+    form = forms.SignUpForm()
+
+    if flask.request.method == "POST":
+        if form.validate_on_submit():
+            username = form.username.data
+            password = form.password.data
+            print(f"{username} is trying to register with password {password}")
+
+    return flask.render_template("signup.html", form=form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
