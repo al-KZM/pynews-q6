9e02bfdd31ef08094bc30829355a4106e4e0ee8e
x41lakazam <x41lakazam@gmail.com>
2021-04-12 22:13:48 +0300

    routine

--- webapp/models.py
+++ webapp/models.py
@@ -12,11 +12,11 @@ def user_loader(user_id):
 class User(db.Model, flask_login.UserMixin): # db.Model is required if you want to create an SQL model
     """
     user
-    +-----------+----------------+--------------------+------------------------------------+
-    |  id (PK)  |  name (str64)  |  password (str64)  |  fav_quote (int) --> FK to Quote   |
-    +-----------+----------------+--------------------+------------------------------------+
-    |           |                |                    |                                    |
-    +-----------+----------------+--------------------+------------------------------------+
+    +-----------+----------------+--------------------+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+--------------------+
+    |  id (PK)  |  name (str64)  |  password (str64)  |  fav_quote (int) --> FK to Quote   | fav_quote_id (BTS) |
+    +-----------+----------------+--------------------+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+--------------------+
+    |           |                |                    |          <Quote> object            |        1           |
+    +-----------+----------------+--------------------+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+--------------------+
     """
 
     # Every attribute is a class variable
@@ -27,13 +27,16 @@ class User(db.Model, flask_login.UserMixin): # db.Model is required if you want 
     password = db.Column(db.String(64))
 
 
+    fav_quote = db.relationship('Quote', backref="user", uselist=False) # uselist=False <--> OneToOne relationship
+
+
 class Quote(db.Model):
     """
     quote
     +-----------+-------------------+--------------------+--------------------+---------------------------------+
     |  id (PK)  |  sentence(str64)  |    author (str64)  |   date (datetime)  |    user_id(int) --> FK to User  |
     +-----------+-------------------+--------------------+--------------------+---------------------------------+
-    |           |                   |                    |                    |                                 |
+    |           |                   |                    |                    |               3                 |
     +-----------+-------------------+--------------------+--------------------+---------------------------------+
     """
 
@@ -42,3 +45,5 @@ class Quote(db.Model):
     sentence = db.Column(db.String(256), nullable=False)
     author   = db.Column(db.String(64), nullable=True)
     date     = db.Column(db.DateTime(), nullable=True)
+
+    user_id  = db.Column(db.Integer(), db.ForeignKey("user.id"))
