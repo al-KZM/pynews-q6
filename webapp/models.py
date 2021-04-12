f76a4d8eeccb5073f8705177bb3d857f68ba39d2
x41lakazam <x41lakazam@gmail.com>
2021-04-12 22:02:49 +0300

    routine

--- webapp/models.py
+++ webapp/models.py
@@ -12,11 +12,11 @@ def user_loader(user_id):
 class User(db.Model, flask_login.UserMixin): # db.Model is required if you want to create an SQL model
     """
     user
-    +-----------+----------------+--------------------+
-    |  id (PK)  |  name (str64)  |  password (str64)  |
-    +-----------+----------------+--------------------+
-    |           |                |                    |
-    +-----------+----------------+--------------------+
+    +-----------+----------------+--------------------+------------------------------------+
+    |  id (PK)  |  name (str64)  |  password (str64)  |  fav_quote (int) --> FK to Quote   |
+    +-----------+----------------+--------------------+------------------------------------+
+    |           |                |                    |                                    |
+    +-----------+----------------+--------------------+------------------------------------+
     """
 
     # Every attribute is a class variable
@@ -40,5 +40,5 @@ class Quote(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
 
     sentence = db.Column(db.String(256), nullable=False)
-    author   = db.Column(db.String(64))
-    date     = db.Column(db.DateTime())
+    author   = db.Column(db.String(64), nullable=True)
+    date     = db.Column(db.DateTime(), nullable=True)
