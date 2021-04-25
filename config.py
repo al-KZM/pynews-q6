9588b9f02ac426b113745350ef1961af46f0a559
x41lakazam <x41lakazam@gmail.com>
2021-04-25 23:06:01 +0300

    routine

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 
 # https://flask.palletsprojects.com/en/1.1.x/config/
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
 
     DEBUG = True
+    SECRET_KEY = "my-very-secret-key"
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    BASEDIR = basedir
 
 
