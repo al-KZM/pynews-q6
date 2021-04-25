9549d6d1c13720a4b3d0c3a5a52c5a023e87ece3
x41lakazam <x41lakazam@gmail.com>
2021-04-25 22:48:19 +0300

    routine

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -3,31 +3,36 @@ import flask_sqlalchemy
 import flask_migrate
 import flask_login
 
-app = flask.Flask(__name__)
-
-# In case you have: "A secret key is required to use..."
-app.config["SECRET_KEY"] = "my-very-secret-key"
-
-# Format of a postgresSQL database url:
-# postgresql://<username>:<password>@<hostname>:<port>/<db_name>
-
-
-# WORKAROUND FOR DB ISSUES:
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/pynews"
 import os
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.db')
-# END OF WORKAROUND
 
-db = flask_sqlalchemy.SQLAlchemy(app)    # database bridge
-migrate = flask_migrate.Migrate(app, db) # Migrator
-login_manager = flask_login.LoginManager(app)
 
-################ END OF THE APP CONFIG
+db = flask_sqlalchemy.SQLAlchemy()              # database bridge
+migrate = flask_migrate.Migrate()               # Migrator
+login_manager = flask_login.LoginManager()
 
-from .auth import auth_blueprint
-from . import routes, models, filters
 
-app.register_blueprint(auth_blueprint, url_prefix="/auth")
+def create_app():
+
+    app = flask.Flask(__name__)
+
+    from .auth import auth_blueprint
+    from . import routes, models, filters
+
+    app.config["SECRET_KEY"] = "my-very-secret-key"
+
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/pynews"
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
+    db.init_app(app)
+    migrate.init_app(db=db, app=app)
+    login_manager.init_app(app)
+
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
+
+    return app
 
 
