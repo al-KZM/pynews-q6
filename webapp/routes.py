a09a09d76879eb2b8e1dd0096bb2cb9dd0609330
x41lakazam <x41lakazam@gmail.com>
2021-04-25 22:03:20 +0300

    routine

--- webapp/routes.py
+++ webapp/routes.py
@@ -36,62 +36,6 @@ def search_article():
     # case 2: Get request --> the user just wants to see the page
     return flask.render_template("search_article.html", form=form)
 
-# Sign up page
-
-# Create a page that displays a sign up form (username & password) and when it gets data from a user
-# just print "Rick is signing up with password chocolate"
-
-# The password needs to contain 6 to 12 characters
-@app.route("/sign-up", methods=["GET","POST"])
-def signup():
-    """
-    The function needs to add the user to the DB
-    :return:
-    """
-    form = forms.SignUpForm()
-
-    if flask.request.method == "POST":
-        if form.validate_on_submit():
-            username = form.username.data
-            password = form.password.data
-
-            # Create user
-            user = models.User(name=username, password=password)
-            # Add it to the DB
-            db.session.add(user)
-            # Commit your changes
-            db.session.commit()
-            print(f"{username} was registered successfully")
-
-    return flask.render_template("signup.html", form=form)
-
-@app.route("/sign-in", methods=["GET", "POST"])
-def signin():
-    form = forms.SignInForm()
-
-    if flask.request.method == "POST":
-        if form.validate_on_submit():
-            username = form.username.data
-            password = form.password.data
-
-            # Retrieve the user that matches this username
-            user = models.User.query.filter_by(name=username).first()
-
-            # Check the provided password against the user's one
-            if user is not None and user.password == password:
-                flask_login.login_user(user)
-                flask.flash("User logged in successfully !", "success")
-            else:
-                flask.flash("Something went wrong.", "danger") # Put the message into the flashed messages
-                # To retrieve those messages: flask.get_flashed_messages()
-
-    return flask.render_template("signin.html", form=form)
-
-@app.route("/sign-out")
-def signout():
-    flask_login.logout_user()
-    return flask.redirect('/')
-
 
 # Create a route that displays a list of all the registered users
 @app.route("/users")
