c1db8744d892f3682ff8f306ce08eb83d3a7354b
x41lakazam <x41lakazam@gmail.com>
2021-04-25 23:47:01 +0300

    routine

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,23 @@ import config
 
 
 app = create_app(config.current_config)
+
+
+
+def inject_variables_to_shell():
+    """
+    Return a dictionary of variables to inject ({variable_name: variable_value})
+    Those variables will be accessible in the flask shell (without having to import them)
+    """
+    from webapp.auth.models import User
+    from webapp.main.models import Quote, Book
+    from webapp import db
+    return {
+        "User": User,
+        "Quote": Quote,
+        "Book": Book,
+        "db": db
+    }
+
+
 app.run(port=5000, debug=True)
