a131ab97c3e9d1a8c1c178e78c6cce18f6e040a7
x41lakazam <x41lakazam@gmail.com>
2021-05-02 23:49:36 +0300

    routine

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -42,5 +42,5 @@ def get_locale():
     # otherwise try to guess the language from the user accept
     # header the browser transmits.  We support de/fr/en in this
     # example.  The best match wins.
-    return flask.request.accept_languages.best_match(['de', 'fr', 'en'])
+    return flask.request.accept_languages.best_match(['he', 'sp', 'fr', 'en'])
 
