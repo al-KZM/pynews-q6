bd80abd61087a5fd584609172dcf2f7e02efe631
x41lakazam <x41lakazam@gmail.com>
2021-05-02 21:57:34 +0300

    routine

--- config.py
+++ config.py
@@ -6,18 +6,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # https://flask.palletsprojects.com/en/1.1.x/config/
 class Config:
 
+    BASEDIR = basedir
+    UPLOADS_DIR = os.path.join(basedir, 'webapp/uploads')
+
+
+class DevConfig(Config):
+    """
+    Development config
+    """
+
     DEBUG = True
     SECRET_KEY = "my-very-secret-key"
 
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-    BASEDIR = basedir
-    UPLOADS_DIR = os.path.join(basedir, 'webapp/uploads')
 
-
-    # MAIL CONFIG
-
-    # pip install flask-mail
     MAIL_SERVER  = "smtp.gmail.com" # mail.yahoo.fr
     MAIL_PORT    = 587 # 465
     MAIL_USE_TLS = True
@@ -26,28 +29,32 @@ class Config:
     MAIL_USERNAME       = 'elmozarello@gmail.com'
     MAIL_DEFAULT_SENDER = 'elmozarello@gmail.com'
 
-    # Use an env variable:
-    # In your terminal:
-    # OSX: $ export MAIL_PASSWORD='mypassword' (To make it permanent, put this line in ~/.bash_profile)
-    # WIN: Use cmder
-
     # Somewhere you need to do (probably in ~/.bash_profile):
     # export MAIL_PASSWORD="mypassword"
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 
-class DevConfig(Config):
-    """
-    Development config
-    """
-    pass
-
 class ProdConfig(Config):
     """
     Production config
     """
+    DEBUG = False
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres:postgres@localhost:5432/pynews"
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    MAIL_SERVER  = "smtp.gmail.com" # mail.yahoo.fr
+    MAIL_PORT    = 587 # 465
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    MAIL_USERNAME       = 'elmozarello@gmail.com'
+    MAIL_DEFAULT_SENDER = 'elmozarello@gmail.com'
+
+    # Somewhere you need to do (probably in ~/.bash_profile):
+    # export MAIL_PASSWORD="mypassword"
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
 
 
 
